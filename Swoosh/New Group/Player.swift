d5a3381707f7f262f1ee6a8a8fe759731df6988d
Muhriddin Ismoilov <ismoilovmuh1996@gmail.com>
2018-04-27 17:55:26 +0500

    model created for player

---
+++ Swoosh/New Group/Player.swift
@@ -0,0 +1,14 @@
+ //
+//  Player.swift
+//  Swoosh
+//
+//  Created by Muhriddin on 4/27/18.
+//  Copyright © 2018 Muhriddin. All rights reserved.
+//
+
+import Foundation
+
+ struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+ }
